45cefbe6135edf02f6431ffb6b3e901ba08039ec
Chich-ai <Mlopesdasilvacoronetti@gmail.com>
2023-06-26 21:40:30 -0300

    Aula 26/06
    minha nossa

---
+++ senac_junit/src/main/java/org/example/ParImpar.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class ParImpar {
+    public static boolean ePar(int x){
+        return x % 2 == 0;
+    }
+
+    public static boolean eImpar(int x) {
+        return x % 2 != 0;
+    }
+}
---
+++ senac_junit/src/test/java/org/example/CalculadoraTest.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculadoraTest {
+
+    @org.junit.jupiter.api.Test
+    void soma() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void subtracao() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void divisao() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void multiplicacao() {
+    }
+}
