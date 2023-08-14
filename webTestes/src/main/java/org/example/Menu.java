cc9a77590f289cd6bdcfabaf4bd6890901841206
Chich-ai <Mlopesdasilvacoronetti@gmail.com>
2023-08-14 20:00:25 -0300

    Menu
    aproveitei e coloquei uma letra de música

---
+++ webTestes/src/main/java/org/example/Menu.java
@@ -0,0 +1,35 @@
+package org.example;
+public class Menu {
+    public static void main(String[] args) {
+        System.out.println("BUG (English cover) - Will Stetson");
+        System.out.println("Can’t be seen,\n" +
+                "I’m lost within this dark scene ");
+        System.out.println("(pa-pa-para paranoia)\n" +
+                "Wack and wack, ");
+        System.out.println("my heart is whacked in one hack \n" +
+                "(pa-pa-para paranoia)");
+        System.out.println("Can’t escape, \n" +
+                "I’m tangled in this one place ");
+        System.out.println("(pa-pa-para paranoia)\n" +
+                "Sad, so sad, ");
+        System.out.println("I’m lying on my own back \n" +
+                "(pa-pa-para paranoid ah)");
+        System.out.println("Uh, bu-bu-buggy, bug, buggy, buggy\n" +
+                "Ta-ta-tagging, tagged ability");
+        System.out.println("Oh, ru-ru-rubbing feelings off of me\n" +
+                "This show of this “pain” grows, ");
+        System.out.println("oh no, oh no, oh no\n" +
+                "Oh no, oh no, oh no!");
+        System.out.println("Well! Drop down with fear all around, that pa-paranoia!\n" +
+                "I’ll raise it loud, my voice that drowned, I’m spinning far out");
+        System.out.println("Well! I know I’ll fall even though I’ll try and I’ll hope to float!\n" +
+                "I’m sinking low for sure, so \n" +
+                "oh no, oh no, oh NO!");
+        System.out.println("Uh, bu-bu-buggy, bug, buggy, buggy\n" +
+                "Your love is done so wrongly, \n" +
+                "my hate is growing deep");
+        System.out.println("Uh, bu-bu-buggy, bug, buggy, buggy\n" +
+                "That answer came off so weak, \n" +
+                "my hate is growing deep...");
+    }
+}
