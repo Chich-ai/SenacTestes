3c1632553e08001dcf6d1bacc1d5ad9a49b1b7e1
Chich-ai <Mlopesdasilvacoronetti@gmail.com>
2023-08-15 19:19:11 -0300

    coisas q eu esqueci
    sla de onde veio isso

---
+++ teste1/src/com/senac/bugs/CreateListOfStrings.java
@@ -0,0 +1,14 @@
+package com.senac.bugs;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CreateListOfStrings {
+
+    public static void main(String[] args) {
+        List<String> strings = new ArrayList<>();
+        strings.add("Hello World");
+        System.out.println(strings);
+    }
+}
+
---
+++ webTestes/src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        //calculadora (+ - / *)
+        //banco (saque e depósito)
+        //menu (enquanto não sair, dar opções)
+        //triângulo (pedir lados e informar o triângulo)
+
+        System.out.println();
+    }
+}
